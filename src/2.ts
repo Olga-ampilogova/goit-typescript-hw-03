7837c2d7fd035178c8775bb5398d9599ef1b2054
Olga-ampilogova <141365234+Olga-ampilogova@users.noreply.github.com>
2024-04-13 14:36:09 +0200

    Update hometask

--- src/2.ts
+++ src/2.ts
@@ -14,11 +14,32 @@
 
 */
 
+// class Employee {
+//   // Заповніть модифікатори доступу
+//   name: string;
+//   department: string;
+//   salary: number;
+
+//   constructor(name: string, department: string, salary: number) {
+//     this.name = name;
+//     this.department = department;
+//     this.salary = salary;
+//   }
+
+//   getEmployeeDetails() {
+//     return `Name: ${this.name}, Department: ${this.department}, Salary: ${this.salary}`;
+//   }
+// }
+
+// class Manager extends Employee {
+//   // Реалізуйте конструктор та збільшіть salary на 10000
+// }
+
+
 class Employee {
-  // Заповніть модифікатори доступу
-  name: string;
-  department: string;
-  salary: number;
+  public name: string;
+  protected department: string;
+  private salary: number;
 
   constructor(name: string, department: string, salary: number) {
     this.name = name;
@@ -32,8 +53,9 @@ class Employee {
 }
 
 class Manager extends Employee {
-  // Реалізуйте конструктор та збільшіть salary на 10000
+  constructor(name: string, department: string, salary: number) {
+    super(name, department, salary + 10000);
+  }
 }
-
 
 export {};
--- src/4.ts
+++ src/4.ts
@@ -1,10 +1,80 @@
-const key = new Key();
+// const key = new Key();
 
+// const house = new MyHouse(key);
+// const person = new Person(key);
+
+// house.openDoor(person.getKey());
+
+// house.comeIn(person);
+class Key {
+  private signature: number;
+
+  constructor() {
+    this.signature = Math.random();
+  }
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  private key: Key;
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean;
+  protected key: Key;
+  protected tenants: Person[];
+
+  constructor(door: boolean, key: Key) {
+    this.door = door;
+    this.key = key;
+    this.tenants = [];
+  }
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log('Person entered the house.');
+    } else {
+      console.log('The door is closed.');
+    }
+  }
+
+  abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  constructor(key: Key) {
+    super(true, key);
+  }
+
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log('The door is open.');
+    } else {
+      console.log('Cannot open the door. Invalid key.');
+    }
+  }
+}
+
+// Створення ключа, будинку та особи
+const key = new Key();
 const house = new MyHouse(key);
 const person = new Person(key);
 
+// Відкриття дверей і впускання особи в будинок
 house.openDoor(person.getKey());
-
 house.comeIn(person);
 
 
